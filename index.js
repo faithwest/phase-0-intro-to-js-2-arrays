59e049e312acda765e2daad8e4e1a9630d809365
faithwest <faithchepkoech425@gmail.com>
2023-09-29 14:59:44 +0300

    complete

---
+++ index.js
@@ -0,0 +1,19 @@
+/*create array and access thru console.log/bracket notation
+let cars = ['range','bmw','raptor','porsche']
+console.log(cars[1]);
+console.log(cars[3]);
+console.log(cars[2]);
+console.log(cars[o]);*/
+
+
+//mutate elements
+let cars = ['range','bmw','raptor','porsche']
+cars[0] = '0'
+cars[3] = '1'
+cars[2] = 'true'
+console.log(cars)
+
+//array methods:CAN ONLY be called on arrays.they allow manipulation
+let string = 'nairobi'
+string.toUpperCase();
+//=>'NAIROBI'*/
